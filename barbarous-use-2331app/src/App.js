939e79b86afb377d78738ef276c31ed53a451bee
Mohammadzaki0027 <zaki7875@gmail.com>
2022-07-20 00:38:49 +0530

    firstcommit

---
+++ barbarous-use-2331app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import {Box} from "@chakra-ui/react"
+function App() {
+  return (
+   <Box border="1px solid red" width="99%" margin="auto" height="100px">
+
+
+
+    
+   </Box>
+  );
+}
+
+export default App;
