65b148901f24837b0eba04851ae30b0bb83da6d9
Mohammadzaki0027 <zaki7875@gmail.com>
2022-07-21 02:16:06 +0530

    footer

--- barbarous-use-2331app/src/Component/Navbar.jsx
+++ barbarous-use-2331app/src/Component/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Box, Container, Flex, HStack,Stack,Select } from '@chakra-ui/react'
 import {Link} from "react-router-dom"
+import styles from "../Styles/Navbar.module.css"
 import React from 'react'
 const link = [
 
@@ -15,21 +16,25 @@ const link = [
 ];
 const Navbar = () => {
   return (
-  <Box backgroundColor="teal" width="100%">
+  <Box backgroundColor="black" width="100%">
     <Flex justifyContent="space-around" >
-<HStack >
- 
-  </HStack>
+
 <HStack spacing="40px">
-  <Box><img src="https://www.cricket.com/cricket.com.svg" alt="" /></Box>
+  <Box>
+    <Link to="/">
+    
+    <img src="https://www.cricket.com/cricket.com.svg" alt="" />
+    </Link>
+    
+    </Box>
  {link.map((e)=>(
-<Box>
-  <Link to={e.to}>{e.element}</Link>
+<Box key={e.to}>
+  <Link to={e.to} className={styles.link}>{e.element}</Link>
 </Box>
  ))}
  <Box>
- <Select placeholder="More" width="55%">
-  <option value='option1'>Stadium</option>
+ <Select placeholder="More" width="55%"  className={styles.selectatg}>
+  <option value='option1'>Stadium</option >
   <option value='option2'>Ranking</option>
   <option value='option3'>Archive</option>
 
--- barbarous-use-2331app/src/Routes/Allroutes.jsx
+++ barbarous-use-2331app/src/Routes/Allroutes.jsx
@@ -6,13 +6,13 @@ import Schedule from "../Pages/Schedule"
 import News from "../Pages/News"
 import Video from "../Pages/Video"
 import Player from "../Pages/Player"
-
+import Home from '../Pages/Home'
 
 const Allroutes = () => {
   return (
     <>
     <Routes>
-    {/* <Route path='/' element={<App></App>}></Route> */}
+    <Route path='/' element={<Home></Home>}></Route>
             <Route path='/FantacyCenter' element={<FantacyCenter></FantacyCenter>}></Route>
          <Route path='/Criclytics' element={<Criclytics></Criclytics>}></Route>
             <Route path='/Schedule' element={<Schedule></Schedule>}></Route>
